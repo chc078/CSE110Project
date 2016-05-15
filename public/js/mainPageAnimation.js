5d48aa434e70d0da6201a12e5a3814cb106668e6
Yuxiang <guanyx2007@hotmail.com>
2016-05-15 12:30:28 -0700

    API added

--- public/js/mainPageAnimation.js
+++ public/js/mainPageAnimation.js
@@ -23,55 +23,28 @@ $('#forgotten').click(function(){
         $("#forgotten-container").fadeIn();
     });
 });
-var formData;
+
 $(".input-group").keypress(function(e) {
+    var formData;
     if(e.which == 13) {
         formData = $(".input-group").find('input[name="name"]').val();
         console.log(formData);
         var xhr = new XMLHttpRequest();
         xhr.open("GET", "http://api.yummly.com/v1/api/recipes?_app_id=f690d55a&_app_key=5a8f8f5fd0032df11eefecbe8dda2dbc&q="+formData, false);
         xhr.send();
-        var responseString=JSON.parse(xhr.responseText)
-        console.log(responseString);
+        var responseString=JSON.parse(xhr.responseText);
+        //console.log(responseString);
+        getMatches(responseString.matches);
     }
     
 });
-/*
-// Helper function to display JavaScript value on HTML page.
-function showResponse(response) {
-    var responseString = JSON.stringify(response, '', 2);
-    document.getElementById('response').innerHTML += responseString;
-}
-
-// Called automatically when JavaScript client library is loaded.
-/*
-function onClientLoad() {
-    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
-}*/
-
-// Called automatically when YouTube API interface is loaded (see line 9).
-/*
-function onYouTubeApiLoad() {
-    // This API key is intended for use only in this lesson.
-    // See https://goo.gl/PdPA1 to get a key for your own applications.
-    //gapi.client.setApiKey('AIzaSyCR5In4DZaTP6IEZQ0r1JceuvluJRzQNLE');
-
-    search();
-}
-
-function search() {
-    // Use the JavaScript client library to create a search.list() API call.
-    var request = gapi.client.youtube.search.list({
-        part: 'snippet',q:"csgo"
+function getMatches(arr) {
+    var out = "";
+    var i;
+    for(i = 0; i < arr.length; i++) {
+        out += '<li>' + arr[i].recipeName + '</li><br>';
         
-    });
-    
-    // Send the request to the API server,
-    // and invoke onSearchRepsonse() with the response.
-    request.execute(onSearchResponse);
+    }
+    console.log(document.getElementById("response"));
+    document.getElementById("response").innerHTML = out;
 }
-
-// Called automatically with the response of the YouTube API request.
-function onSearchResponse(response) {
-    showResponse(response);
-}*/
