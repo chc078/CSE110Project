0b41c3f322f8039d2a5fbd210fac7458434ae871
Yuxiang <guanyx2007@hotmail.com>
2016-05-26 01:21:29 -0700

    nutrition function added, though only dummy static values

--- public/js/mainPageAnimation.js
+++ public/js/mainPageAnimation.js
@@ -23,28 +23,47 @@ $('#forgotten').click(function(){
         $("#forgotten-container").fadeIn();
     });
 });
-
+function getRecipe(id) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://api.yummly.com/v1/api/recipe/" + id + "?_app_id=f690d55a&_app_key=5a8f8f5fd0032df11eefecbe8dda2dbc", false);
+    xhr.send();
+    var responseString=JSON.parse(xhr.responseText);
+    getNutrition(responseString);
+    
+}
 $(".input-group").keypress(function(e) {
     var formData;
-    var unirest = require('unirest');
-    if(e.which == 13) {
+    //var unirest = require('unirest');
+    if(e.which === 13) {
         formData = $(".input-group").find('input[name="name"]').val();
         console.log(formData);
-        /*
+        
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://api.yummly.com/v1/api/recipes?_app_id=f690d55a&_app_key=5a8f8f5fd0032df11eefecbe8dda2dbc&q="+formData, false);
+        xhr.open("GET", "http://api.yummly.com/v1/api/recipes?_app_id=f690d55a&_app_key=5a8f8f5fd0032df11eefecbe8dda2dbc&q="+ formData, false);
         xhr.send();
-        var responseString=JSON.parse(xhr.responseText);
+        var responseString = JSON.parse(xhr.responseText);
         //ID can be used to GET the full recipe details.
         //&requirePictures=true  // return recipes with photos
         //console.log(responseString);
-        getMatches(responseString.matches);*/
+        getMatches(responseString.matches);
+        /*
         unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?&query"+formData).header("X-Mashape-Key","ps6oh8JkV7mshe8Kb0JRofZ8iUkxp18bMGwjsniAae2wPEU3us").end(function (result) {
             console.log(result.status, result.headers, result.body);
-        });
+        });*/
+        getRecipe("Vegetarian-Cabbage-Soup-Recipezaar");
     }
     
 });
+
+function getNutrition(arr){
+    var nutrition = "";
+    var i;
+    var nutrition_vec = arr.nutritionEstimates;
+    for(i = 0;i<nutrition_vec.length;i++){
+        nutrition+=nutrition_vec[i].description+":"+nutrition_vec[i].value+" "+nutrition_vec[i].unit.pluralAbbreviation+"\n";
+    }
+    console.log(nutrition);
+}
 function getMatches(arr) {
     var out = "";
     var i;
@@ -53,11 +72,11 @@ function getMatches(arr) {
         
     }
     console.log(document.getElementById("response"));
-    
-    document.open("~/views/SearchResults.jade"); //Replace??
+    /*
+    document.open("~/views/SearchResults.html"); //Replace??
 	document.write("<html><body><p>Hello World!</p></body></html>");
 	document.close();
     
     //document.getElementById("response").innerHTML = out;
-    window.location.href = "../../views/searchResults.jade";
+    window.location.href = "../../views/searchResults.jade";*/
 }
