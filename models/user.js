d8edeb9004f3fadecb05655b02babbf74feba932
Xavier Wang <xiw180@ucsd.edu>
2016-06-04 22:24:38 -0700

    all done 1.11

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var userSchema = new mongoose.Schema({
    username: { type: String, unique: true},
    password: {type: String},
-   email: {type: String},
+   email: {type: String, unique: true},
    resetPasswordToken: {type:String},
    resetPasswordExpire: {type : Date},
    id: String,
